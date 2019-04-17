a4989f1cadd9d24bfed35a4665b243e72dc67650
Didac Bigorda <didacbigorda@gmail.com>
2019-04-17 18:17:07 +0200

    Add package.json, webpack config and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['@babel/polyfill', 'whatwg-fetch', './app/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ]}
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  node: {
+    fs: 'empty' // Since dotenv is not running on Node.js
+  }
+}
