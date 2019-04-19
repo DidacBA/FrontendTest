1971734be4e477d6591d2535d6ee926e43c8ddd1
Didac Bigorda <didacbigorda@gmail.com>
2019-04-19 16:17:26 +0200

    Add api tests

--- app/utils/api.js
+++ app/utils/api.js
@@ -3,21 +3,31 @@ const sec = process.env.SECRET_ID;
 const params = `?client_id=${id}&client_secret=${sec}`;
 
 export async function getProfile (username) {
-  const response = await fetch(`https://api.github.com/users/${username}${params}`);
-  
-  return response.json();
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}${params}`);  
+    return response.json();
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function getRepos (username) {
-  const response = await fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
-
-  return response.json();
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
+    return response.json();
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function getProfileAndRepos (username) {
-  const profile = await getProfile(username);
-  const repos = await getRepos(username);
-  const response = { profile, repos };
-
-  return response;
+  try {
+    const profile = await getProfile(username);
+    const repos = await getRepos(username);
+    const response = { profile, repos };
+  
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
--- tests/api.test.js
+++ tests/api.test.js
@@ -8,8 +8,15 @@ test('returns correct Github profile', async () => {
   await expect(profile.login).toEqual('DidacBA');
 })
 
-test('returns repos object with user repos', async () => {
+test('returns repos object with correct user repos', async () => {
   const repos = await getRepos('DidacBA');
 
-  
+  await expect(repos[0].name).toBe('Alexandria');
+})
+
+test('returns object with correct user profile and repos', async () => {
+  const profileAndRepos = await getProfileAndRepos('DidacBA');
+
+  await expect(profileAndRepos.profile.login).toEqual('DidacBA');
+  await expect(profileAndRepos.repos[0].name).toBe('Alexandria');
 })
