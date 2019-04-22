78fd60311b19a8171954396e62fc11b74167d481
Didac Bigorda <didacbigorda@gmail.com>
2019-04-22 14:23:17 +0200

    Add DOMScrubber test

--- tests/DOM.test.js
+++ tests/DOM.test.js
@@ -1,4 +1,4 @@
-import { element, grab } from '../app/utils/DOMHelp/DOM';
+import { element, grab, DOMScrubber } from '../app/utils/DOMHelp/DOM';
 
 test('createElement returns an html element with correct attributes', () => {
   const mockElement = element({
@@ -23,4 +23,18 @@ test('grab selects the correct HTML element', () => {
     </div>`;
 
   expect(grab('text').id).toEqual('text');
+})
+
+test('erases selected nodes from DOM', () => {
+  document.body.innerHTML = 
+    `<div id="container">
+      <span id="text" />
+      <button id="button" />
+    </div>`;
+
+  expect(grab('text').id).toEqual('text');
+
+  DOMScrubber(['text']);
+
+  expect(grab('text')).toEqual(null);
 })
